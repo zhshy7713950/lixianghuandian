aeca021aa389f4236da84d36bb84f017f794ed8a
wangjc <wlwangjc@foxmail.com>
2024-03-29 20:32:43 +0800

    修改资源

--- app/src/main/java/com/ruimeng/things/me/FgtMeDeposit.kt
+++ app/src/main/java/com/ruimeng/things/me/FgtMeDeposit.kt
@@ -1,11 +1,13 @@
 package com.ruimeng.things.me
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.flyco.dialog.listener.OnBtnClickL
 import com.flyco.dialog.widget.NormalDialog
 import com.flyco.roundview.RoundTextView
@@ -13,12 +15,15 @@ import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog
 import com.ruimeng.things.FgtMain
 import com.ruimeng.things.PathV3
 import com.ruimeng.things.R
+import com.ruimeng.things.ScanQrCodeActivity
 import com.ruimeng.things.home.FgtHome
 import com.ruimeng.things.home.bean.ChangeRentBatteryBean
 import com.ruimeng.things.home.bean.ChangeRentBatteryPayInfoBean
 import com.ruimeng.things.home.bean.PaymentDetailBean
 import com.ruimeng.things.showConfirmDialog
 import com.utils.TextUtil
+import com.utils.ToastHelper
+import com.uuzuche.lib_zxing.activity.CodeUtils
 import com.xianglilai.lixianghuandian.wxapi.WXEntryActivity
 import kotlinx.android.synthetic.main.fgt_me_deposite.tv_battery_hole
 import kotlinx.android.synthetic.main.fgt_me_deposite.tv_battery_num
@@ -30,7 +35,10 @@ import wongxd.alipay.BaseAlipay
 import wongxd.base.BaseBackFragment
 import wongxd.base.custom.anylayer.AnyLayer
 import wongxd.common.EasyToast
+import wongxd.common.getCurrentAty
 import wongxd.common.getSweetDialog
+import wongxd.common.permission.PermissionType
+import wongxd.common.permission.getPermissions
 import wongxd.common.toPOJO
 import wongxd.http
 
@@ -60,8 +68,11 @@ class FgtMeDeposit : BaseBackFragment() {
         getInfo()
     }
 
-
+    var virtaul = false
     private fun getInfo() {
+        if (FgtHome.CURRENT_DEVICEID.startsWith("8") && FgtHome.CURRENT_DEVICEID.length == 8 ){
+            virtaul = true
+        }
         tv_battery_num.text = "电池编号："+FgtHome.CURRENT_DEVICEID
         var textColors = arrayOf("#929FAB","#FFFFFF")
         var type = when (FgtHome.payType){
@@ -80,24 +91,114 @@ class FgtMeDeposit : BaseBackFragment() {
 
         tv_battery_status.text = TextUtil.getSpannableString(arrayOf("支付渠道：",type),textColors)
         tv_battery_hole.text = TextUtil.getSpannableString(arrayOf("电池押金：",deposit),textColors)
+        if (virtaul){
+            if (FgtHome.payType == "101"){
+                tv_remark.text = "解绑免押申请通过后，将自动解除免押绑定"
+                tv_deposit_return.text ="申请解绑免押"
+            }else{
+                tv_remark.text = "退还押金申请通过后，1-2个工作日到账"
+                tv_deposit_return.text ="申请退还押金"
+            }
+            var dialogTitle = if (FgtHome.payType == "101") "免押解绑结束后，剩余套餐将清零，请确认操作！"
+            else "押金退还结束后，剩余套餐将清零，请确认操作！"
+            var dialogDesc = if (FgtHome.payType == "101") "请确认是否解绑免押" else "请确认是否退还押金"
+            tv_deposit_return.setOnClickListener {
+                NormalDialog(activity)
+                    .apply {
+                        style(NormalDialog.STYLE_TWO)
+                        btnNum(2)
+                        title(dialogTitle)
+                        content(dialogDesc)
+                        btnText("确认", "取消")
+                        setOnBtnClickL(OnBtnClickL {
+                            dismiss()
+                            http {
+                                url = "/apiv6/payment/getuserpaymentinfo"
+                                params["user_id"] = FgtHome.userId
+                                params["device_id"] = FgtHome.CURRENT_DEVICEID
+                                IS_SHOW_MSG = false
+                                onSuccess { res ->
+                                    var  paymentDetailBean = res.toPOJO<PaymentDetailBean>().data
+                                    tryReturnDeposit(paymentDetailBean.contract_id)
+                                }
+                            }
+                        }, OnBtnClickL {
+                            dismiss()
+                        })
 
-        if (FgtHome.payType == "101"){
-            tv_remark.text = "退还电池成功后将自动解除免押绑定"
+                    }.show()
+
+
+            }
+        }else{
+            tv_deposit_return.text ="立即退租"
+            tv_deposit_return.setOnClickListener {
+                ToastHelper.shortToast(context,"请扫描电柜二维码")
+                getPermissions(getCurrentAty(), PermissionType.CAMERA, allGranted = {
+                    val intent = Intent(activity, ScanQrCodeActivity::class.java)
+                    intent.putExtra("type", "退还")
+                    startActivityForResult(intent, 1)
+                })
+            }
         }
-        tv_deposit_return.setOnClickListener {
-            http {
-                url = "/apiv6/payment/getuserpaymentinfo"
-                params["user_id"] = FgtHome.userId
-                params["device_id"] = FgtHome.CURRENT_DEVICEID
-                IS_SHOW_MSG = false
-                onSuccess { res ->
-                   var  paymentDetailBean = res.toPOJO<PaymentDetailBean>().data
-                    tryReturnDeposit(paymentDetailBean.contract_id)
+
+
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            if (requestCode == 1) {
+                if (null != data) {
+                    val bundle = data.extras ?: return
+                    if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
+
+                        val result = bundle.getString(CodeUtils.RESULT_STRING)
+                        val getType = bundle.getString("type")
+                        val getContractId = bundle.getString("contract_id")
+                        if ("退还" == getType){
+                            if (result != null) {
+                                NormalDialog(activity)
+                                    .apply {
+                                        style(NormalDialog.STYLE_TWO)
+                                        btnNum(2)
+                                        title("提示")
+                                        content("请将电池放入电柜，退还结束后，剩余套餐将清零，请确认操作！")
+                                        btnText("确认", "取消")
+                                        setOnBtnClickL(OnBtnClickL {
+                                            dismiss()
+                                            returnBattery(result)
+                                        }, OnBtnClickL {
+                                            dismiss()
+                                        })
+
+                                    }.show()
+
+                            }
+                        }
+                    }
                 }
             }
-
         }
+    }
+    /**
+     * 柜内租用电池
+     */
+    private fun returnBattery(code: String) {
+        http {
+            url = "apiv6/cabinet/retrunBattery"
+            params["user_id"] = FgtHome.userId
+            params["code"] = code
+            params["device_id"] = FgtHome.CURRENT_DEVICEID
+            onSuccessWithMsg { res, msg ->
+                ToastHelper.shortToast(activity, "请将电池放入电柜，后台自动审核")
 
+            }
+
+            onFail { i, msg ->
+                ToastHelper.shortToast(activity, msg)
+            }
+        }
     }
     private fun tryReturnDeposit(contractId: String) {
 
---
+++ app/src/main/java/com/ruimeng/things/me/view/RebackAlertPopup.kt
@@ -0,0 +1,50 @@
+package com.ruimeng.things.me.view
+
+import android.app.Activity
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import android.widget.TextView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.ruimeng.things.R
+import com.ruimeng.things.me.bean.MyCouponBean
+import org.json.JSONObject
+
+class RebackAlertPopup (private val activity: Activity,
+                        private val coupon: JSONObject,
+                        private val listener: OnClickListener
+) : PopupWindow(activity){
+    init {
+        contentView = View.inflate(activity, R.layout.popup_reback_alert, null)
+        var tv_coupon_price = contentView.findViewById<TextView>(R.id.tv_coupon_price)
+        var tv_coupon_desc = contentView.findViewById<TextView>(R.id.tv_coupon_desc)
+        tv_coupon_price.text = "${coupon.get("coupon_price")}"
+        tv_coupon_desc.text = "租${coupon.get("limit_rent_day")}天可用"
+        width = ViewGroup.LayoutParams.MATCH_PARENT
+        height = ViewGroup.LayoutParams.MATCH_PARENT
+        isOutsideTouchable = true
+        isFocusable = true
+        setBackgroundDrawable(ColorDrawable(0x55000000))
+        contentView.findViewById<TextView>(R.id.tv_exp_remind).setOnClickListener {
+            listener.onClick(it)
+            dismiss()
+        }
+        contentView.findViewById<View>(R.id.rootView).setOnClickListener {
+            listener.onClick(it)
+            dismiss()
+        }
+        contentView.findViewById<View>(R.id.bg).setOnClickListener {
+        }
+
+
+    }
+
+    fun show(view: View) {
+        if (activity.window.decorView.windowToken != null) {
+            showAtLocation(view, Gravity.CENTER, 0, 0)
+        }
+    }
+}
